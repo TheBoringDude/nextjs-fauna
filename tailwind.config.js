ada40c352144ce10c2df16f0be8eb7ed494ce788
Joshue Abance <37932956+TheBoringDude@users.noreply.github.com>
2021-04-15 20:29:11 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: 'jit',
+  // NOTE: configure this if you have another folder with tsx files or (that uses stylinggs)
+  purge: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    // NOTE: Add the lines below if you want to add a google font
+    // fontFamily: {
+    //   sans: ['"name of google font"', 'sans-serif']
+    // },
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
